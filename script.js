f9a134dab1e2ff37bac64b4c9333402d6f1a75bc
CasteAwayy <140751958+CasteAwayy@users.noreply.github.com>
2024-12-04 19:41:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-//your JS code here. If required.
+const inputField = document.querySelectorAll(".code");
+
+inputField[0].focus();
+let cnt = 0;
+
+inputField.forEach((ele) => {
+  ele.addEventListener("keydown", (e) => {
+    e.preventDefault();
+    if (e.key >= 0 && e.key <= 9) {
+      cnt++;
+      e.target.value = e.key;
+      if (cnt < 6) e.target.nextElementSibling.focus();
+      else cnt = 6;
+    }
+    if (e.keyCode == 8) {
+      cnt--;
+      e.target.value = "";
+      if (cnt > 0) e.target.previousElementSibling.focus();
+      else cnt = 0;
+    }
+  });
+});
